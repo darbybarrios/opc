6d375ffc5f0bc7524dc3c236cf578c9a3c8126b7
Darby Barrios <darbybarrios@dlsistemas.com.ve>
2017-01-11 00:11:14 -0400

    Creacion del submenu Deficion Tags Fallas

--- src/main/java/com/opc/repositorio/RepositorioTag.java
+++ src/main/java/com/opc/repositorio/RepositorioTag.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.opc.modelo.Dispositivo;
 import com.opc.modelo.Tag;
@@ -18,5 +20,18 @@ public interface RepositorioTag extends CrudRepository<Tag, Integer> {
 	Tag findBytipoInformacionAndStatRegAndDispositivo(String tipoInf, String statReg, Dispositivo dispositivo);
 	List<Tag> findBytipoInformacionAndStatRegAndDispositivoOrderByItemId(String tipoInf, String statReg, Dispositivo dispositivo);
 	List<Tag> findByDispositivoAndStatRegAndStatusTag(Dispositivo dispositivo, String statReg, String statTag);
+	
+	@Query(value="Select tag.id_tag, tag.desc_tag " +
+				"From " +
+				"tag Inner Join " +
+				"dispositivo " +
+				"On tag.id_dispositivo = dispositivo.id_dispositivo Inner Join " +
+				"maquina " +
+				"On dispositivo.id_maquina = maquina.id_maquina " +
+				"Where " +
+				"maquina.id_maquina = :id_maquina " +
+				"Order By " +
+				"tag.id_tag", nativeQuery=true)
+	List<Object[]> BuscarTagxMaquina(@Param("id_maquina") int idMaquina);
 	
 }
